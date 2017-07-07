6e1d63c524ac4ddc1bbf92d25a59383fa02fd087
YexChen <76753500@qq.com>
2017-07-07 16:25:10 +0800

    添加滑稽版之前

--- pages/hanshu/hanshu.js
+++ pages/hanshu/hanshu.js
@@ -2,8 +2,8 @@
 Page({
   swichpages : function(){
     wx.navigateTo({
-      url: "pages/index/index",
-    })
+      url: "/pages/index/index",
+    });
   },
   onReady: function () {
     var arr = [];
@@ -11,8 +11,14 @@ Page({
     arr.push("喵喵喵"),
     arr.push(23333);
     arr.push("手动滑稽");
+    var a = {
+      name : "颖轩",
+      content : "热水"
+    }
+    arr.push(a);
     for(var i = 0;i < arr.length;i++){
     console.log(arr[i]);
     }
+
   }
 })
